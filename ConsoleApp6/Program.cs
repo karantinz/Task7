f23c1c87ba2c9eceec4b34ac07322ef4bb2f68be
Danila <karantinz@mail.ru>
2021-09-14 10:55:19 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            int total;
+
+            Console.WriteLine("Введите сообщение");
+            userInput = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Сколько раз оно должно повторяться?");
+            total = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < total; i++)
+            {
+                Console.WriteLine(userInput);                
+            }
+            Console.ReadKey();
+        }
+    }
+}
